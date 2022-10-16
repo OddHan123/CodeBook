d7de4d20a9cd9bd688edd9855ddbbf070d6ef2df
OddHan123 <108560003+OddHan123@users.noreply.github.com>
2022-10-16 15:00:17 +0800

    新增資料夾與GCD程式碼

---
+++ Contents/section3/GCD.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int GCD(int x,int y){
+    while(y != 0){
+        return GCD(y,x%y);
+    }
+    return x;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int gcd = GCD(a,b);
+    int lcm = a*b/gcd;
+
+    cout<<"最大公因數為: "<<gcd<<'\n';
+    cout<<"最小公倍數為: "<<lcm<<'\n';
+
+    return 0;
+}
