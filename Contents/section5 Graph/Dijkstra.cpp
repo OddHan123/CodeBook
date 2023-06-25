869c69d6b36a1403ca5c557a737cf4b5d5fde857
OddHan123 <108560003+OddHan123@users.noreply.github.com>
2023-06-25 17:07:26 +0800

    update dijkstra

--- Contents/section5 Graph/Dijkstra.cpp
+++ Contents/section5 Graph/Dijkstra.cpp
@@ -6,8 +6,8 @@ struct Data{
     }
 };
 
-int sol(int s, int e){
-    memset(d,0x3f,sizeof(d));//INF版的memset
+void sol(int s){
+    memset(d,0x3f,sizeof(d));
     memset(vis,0,sizeof(vis));
     d[s] = 0;
     priority_queue<Data> pq;
@@ -16,20 +16,16 @@ int sol(int s, int e){
     while(!pq.empty()){
         Data k = pq.top();
         pq.pop();
-        int ss = k.s;
-        if(ss==e) return d[ss];
-        if(vis[ss]) continue;
-        vis[ss] = 1;
+        int u = k.u;
+        if(vis[u]) continue;
+        vis[u] = 1;
         
-        for(int i=0; i<G[ss].size(); i++){
-            int u = G[ss][i].first, w = G[ss][i].second;
-            if(w>rr) continue;
-            if(d[u][rr-w]>d[ss][rr]){
-                d[u][rr-w] = d[ss][rr];
-                pq.push(Data{u,rr-w,d[u][rr-w]});
+        for(int i=0; i<G[u].size(); i++){
+            int v = G[u][i].first, w = G[u][i].second;
+            if(d[v]>d[u]+w){
+                d[v] = d[u] + w;
+                pq.push(Data{v,d[v]});
             }
         }
-
     }
-    return -1;
 }
