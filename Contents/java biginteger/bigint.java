db98dc5058b13bc1b07b6a615ac14bf4bb22988f
OddHan123 <108560003+OddHan123@users.noreply.github.com>
2023-06-28 20:08:03 +0800

    NG

--- Contents/java biginteger/bigint.java
+++ Contents/java biginteger/bigint.java
@@ -2,27 +2,38 @@ import java.io.*;
 import java.util.*;
 import java.math.BigInteger;
 
-public class bigint {
+public class z {
     public static void main(String args[]) {
         Scanner cin = new Scanner(System.in);
-//Java大數運算宣告BigInteger
-    //首先宣告plus代表做加法運算
-        BigInteger plus = BigInteger.valueOf(0);
-    //首先宣告minus代表做減法運算
-        BigInteger minus = BigInteger.valueOf(0);
-        while ( cin.hasNext() ) {
-    //接下來讀入一整行字串
+//Java大數資料型態: BigInteger
+
+        BigInteger num = BigInteger.valueOf(1);
+        BigInteger btwo = new BigInteger("2");
+        while (cin.hasNext()){
+            BigInteger a = BigInteger.valueOf(0);
+            BigInteger b = BigInteger.valueOf(0);
+
+            //讀入一整行字串
             String str = cin.next();
-        //宣告num代表讀入進來的一整行數字
-        //然後把str丟到BigInteger num裡面
-            BigInteger num = new BigInteger(str);
-            if ( str.equals("0") ) break;
-            else {
-                plus = plus.add(num);
-                minus = minus.subtract(num);
-            }
+
+            //-1停止輸入
+            if (str.equals("-1")) break;
+
+            num = new BigInteger(str);
+            //a += num
+            a = a.add(num);
+
+            //b -= num
+            b = b.subtract(num);
+
+        System.out.print("a+num is " + a + "\n");
+        System.out.print("b-num is " + b + "\n");
+//乘2
+        System.out.printf("%s*2 = %s\n", num, num.multiply(btwo));
+//除2
+        System.out.printf("%s/2 = %s\n\n", num, num.divide(btwo));
         }
-        System.out.print("The plus sum is " + plus + "\n");
-        System.out.print("The minus sum is " + minus + "\n");
+//2的100次方
+        System.out.printf("2^100 = %s\n", btwo.pow(100));
     }
 }
